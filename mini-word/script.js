3dbd697d2565f8b2a38d717d4b454a80ae0540fe
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-02-13 17:47:52 +0100

    Add the possibility to change the font family

--- mini-word/script.js
+++ mini-word/script.js
@@ -1,12 +1,16 @@
 const p = document.querySelector("p");
 const colorInput = document.querySelector(".color");
 const numberInput = document.querySelector(".number");
-const select = document.querySelector("#fonts");
-const value = select.options[select.selectedIndex].value;
+const select = document.querySelector("select");
+const options = document.querySelectorAll("option");
 
 colorInput.addEventListener("input", (e) => {
 	p.style.color = e.target.value;
 });
 numberInput.addEventListener("change", (e) => {
 	p.style.fontSize = e.target.value + "px";
+});
+select.addEventListener("change", (e) => {
+	p.style.fontFamily = select.value;
+	console.log(e);
 });
